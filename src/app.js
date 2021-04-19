952182d542aed71f9a783341b2ee1d3b87abee86
Dhanzman <73815527+Dhanzman@users.noreply.github.com>
2021-04-19 10:13:25 +0000

    correct route error

--- src/app.js
+++ src/app.js
@@ -41,16 +41,16 @@ app.get('/', (req, res) => {
 })
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'about me',
+        title: 'About me',
         name: 'Daniel Sosimi'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        title: 'help',
+        title: 'Help',
         name: 'Daniel Sosimi',
-        message: 'what are you doing to my life'
+        message: 'How can I help you?'
     })
 })
 
